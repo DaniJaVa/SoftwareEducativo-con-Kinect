9fe5d0d63c77afa93a121eac8bbaa02fc272b791
DaniJaVa <danisjv@gmail.com>
2017-04-03 15:20:31 -0500

    Projecto WPF con Kinect

---
+++ CapitalesMexico/MainWindow.xaml.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace CapitalesMexico
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Fields
+
+        List<string> estados = new List<string> { "baja_california_sur", "baja_california", "aguascalientes", "campeche", "coahuila", "colima", "chiapas", "chihuahua",
+                                                    "df", "durango", "guanajuato", "guerrero", "hidalgo","jalisco", "mexico", "michoacan", "morelos", "nayarit", "nuevo_leon",
+                                                    "oaxaca", "puebla", "queretaro", "quintana_roo", "san_luis_potosi", "sinaloa", "sonora", "tabasco", "tamaulipas", "tlaxcala", "veracruz"
+                                                    ,"yucatan", "zacatecas" };
+
+        public Storyboard StoryboardMouseEnter;
+        public Storyboard StoryboardMouseLeave;
+        public Storyboard StoryboardInfo1;
+        public Storyboard StoryboardInfo2;
+
+        string enterColorString = "#FFDCDCDC";
+        string leaveColorString = "#FF727272";
+        string trueAnswerColorString = "#FF09FD1A";
+        string falseAnswerColorString = "#FFFF3A3A";
+
+        XElement xmlFile;
+
+        Random rand = new Random(DateTime.Now.Second.GetHashCode());
+
+        string answer;
+
+        #endregion
+
+        public MainWindow()
+        {
+            try
+            {
+                this.InitializeComponent();
+
+                Closing += OnClosing;
+
+
+                StoryboardMouseEnter = TryFindResource("StoryboardMouseEnter") as Storyboard;
+                StoryboardMouseLeave = (Storyboard)TryFindResource("StoryboardMouseLeave");
+
+                StoryboardInfo1 = TryFindResource("StoryboardInfo1") as Storyboard;
+                StoryboardInfo2 = TryFindResource("StoryboardInfo2") as Storyboard;
+
+                StoryboardInfo1.Completed += new EventHandler(StoryboardInfo1_Completed);
+
+                xmlFile = XElement.Load("XMLestadosInfo.xml");
+
+                changeQuestion();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception", MessageBoxButton.OK);
+                this.Close();
+            }
+        }
+
+        #region MouseEvents
+
+        void country_MouseEnter(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                Path senderPath = (Path)sender;
+
+                changeColorValue(senderPath.Name);
+
+                StoryboardMouseEnter.Begin();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception", MessageBoxButton.OK);
+            }
+        }
+
+        void country_MouseLeave(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                StoryboardMouseLeave.Begin();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception", MessageBoxButton.OK);
+            }
+        }
+
+        private void country_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                Path senderPath = (Path)sender;
+                BrushConverter bc = new BrushConverter();
+
+                //SoundPlayer chord_wav = new SoundPlayer("media\\chord.wav");
+                //SoundPlayer ding_wav = new SoundPlayer("media\\ding.wav");
+
+                if (senderPath.Name == this.answer)
+                {
+                    //ding_wav.Play();
+                    senderPath.Fill = bc.ConvertFromString(trueAnswerColorString) as Brush;
+                    changeQuestion();
+
+                }
+                else
+                {
+                    //chord_wav.Play();
+                    senderPath.Fill = bc.ConvertFromString(falseAnswerColorString) as Brush;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception", MessageBoxButton.OK);
+            }
+        }
+
+        #endregion
+
+        void changeColorValue(string countryName)
+        {
+            try
+            {
+                /////////////////////////////////
+                //changing color of target path
+                Color newColor = (Color)ColorConverter.ConvertFromString(this.enterColorString);
+
+                ColorAnimationUsingKeyFrames colorAnimationUsingKeyFrames = (from c in StoryboardMouseEnter.Children
+                                                                             where (Storyboard.GetTargetName(c) == countryName)
+                                                                             select c).First() as ColorAnimationUsingKeyFrames;
+                colorAnimationUsingKeyFrames.KeyFrames[0].Value = newColor;
+
+                //////////////////////////////////////////////            
+                //changing color of others path except target
+                Color grayColor = (Color)ColorConverter.ConvertFromString(this.leaveColorString);
+
+                var othersColorAnimationUsingKeyFrames = from c in StoryboardMouseEnter.Children
+                                                         where (Storyboard.GetTargetName(c) != countryName)
+                                                         select c;
+
+                foreach (ColorAnimationUsingKeyFrames tmp in othersColorAnimationUsingKeyFrames)
+                    tmp.KeyFrames[0].Value = grayColor;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception", MessageBoxButton.OK);
+            }
+        }
+
+        void updateCountryInfo(string nombreEstado)
+        {
+            try
+            {
+                var countryNode = from c in xmlFile.Descendants(nombreEstado)
+                                  select c;
+
+
+                var Municipios = (from q in countryNode.Descendants("municipios")
+                                  select q).First().Value;
+
+                var Capital = (from q in countryNode.Descendants("capital")
+                               select q).First().Value;
+
+                var flagName = (from q in countryNode.Descendants("flagName")
+                                select q).First().Value;
+
+                //////////////////////////////////////////////
+
+                this.Flag.Source = new BitmapImage(new Uri("escudos\\" + flagName, UriKind.Relative));
+                this.municipios.Text = Municipios;
+                this.capital.Text = Capital;
+
+
+            }
+            catch { }
+        }
+
+        public void changeQuestion()
+        {
+
+            try
+            {
+
+
+                this.answer = estados[rand.Next(0, estados.Count() - 1)];
+                var index = estados.FindIndex(row => row.Contains(this.answer));
+                estados.RemoveAt(index);
+                StoryboardInfo1.Begin();
+
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Felicidades, Has completado el juego!");
+                if (!estados.Any())
+                {
+
+                    estados = new List<string> { "baja_california_sur", "baja_california", "aguascalientes", "campeche", "coahuila", "colima", "chiapas", "chihuahua",
+                                                    "df", "durango", "guanajuato", "guerrero", "hidalgo","jalisco", "mexico", "michoacan", "morelos", "nayarit", "nuevo_leon",
+                                                    "oaxaca", "puebla", "queretaro", "quintana_roo", "san_luis_potosi", "sinaloa", "sonora", "tabasco", "tamaulipas", "tlaxcala", "veracruz",
+                                                    "yucatan", "zacatecas" };
+                    changeQuestion();
+                }
+            }
+        }
+
+        void StoryboardInfo1_Completed(object sender, EventArgs e)
+        {
+            try
+            {
+                updateCountryInfo(this.answer);
+
+                StoryboardInfo2.Begin();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception", MessageBoxButton.OK);
+            }
+        }
+
+        private void OnClosing(object sender, CancelEventArgs cancelEventArgs)
+        {
+            if (MessageBox.Show(this, "¿Estás seguro que quieres salir del juego?", "Confirmación", MessageBoxButton.YesNo, MessageBoxImage.Question) != MessageBoxResult.Yes)
+            {
+                cancelEventArgs.Cancel = true;
+
+            }
+            else
+            {
+                foreach (var process in Process.GetProcessesByName("Microsoft.Kinect.Samples.CursorControl"))
+                {
+                    process.Kill();
+
+
+                }
+            }
+        }
+    }
+}
+
---
+++ K4WHelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect.Toolkit;
+using System.Diagnostics;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit.Controls;
+
+namespace K4WHelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ProcessStartInfo info = new ProcessStartInfo();
+        
+        
+
+        private KinectSensorChooser _sensorChooser;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _sensorChooser = new KinectSensorChooser();
+            _sensorChooser.KinectChanged += SensorChooserOnKinectChanged;
+            SensorChooserUI.KinectSensorChooser = _sensorChooser;
+            _sensorChooser.Start();
+           // MessageBox.Show("Test");
+        }
+
+        private void SensorChooserOnKinectChanged(object sender, KinectChangedEventArgs args)
+        {
+            bool error = false;
+
+            if (args.OldSensor != null)
+            {
+                try
+                {
+                    args.OldSensor.DepthStream.Disable();
+                    args.OldSensor.SkeletonStream.Disable();
+                }
+                catch (Exception)
+                {
+                    //Kinect may enter an invalid state while enabling / disabling strems
+                    //e.g. it might be unplugged
+                    error = true;
+                }
+            }
+
+            if (args.NewSensor == null)
+                return;
+
+            try
+            {
+                args.NewSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+                args.NewSensor.SkeletonStream.Enable();
+                try
+                {
+                    ///NO ES COMPTABILE CON KINECT PARA XBOX
+                    args.NewSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+                    args.NewSensor.DepthStream.Range = DepthRange.Near;
+                    args.NewSensor.SkeletonStream.EnableTrackingInNearRange = true;
+                    //// args.NewSensor.SkeletonFrameReady += NewSensorOnSkeletonFrameReady;
+                }
+                catch (InvalidOperationException)
+                {
+                    args.NewSensor.DepthStream.Range = DepthRange.Default;
+                    args.NewSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                error = true;
+            }
+
+            if (!error)
+                KinectRegion.KinectSensor = args.NewSensor;
+        }
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            _sensorChooser.Stop();
+
+            Microsoft.Samples.Kinect.SpeechBasics.MainWindow ven = new Microsoft.Samples.Kinect.SpeechBasics.MainWindow();
+
+            ven.ShowDialog();
+
+            this.OnLoaded(this, new RoutedEventArgs());
+
+
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            _sensorChooser.Stop();
+
+            //Microsoft.Kinect.Samples.CursorControl.MainWindow cursor = new Microsoft.Kinect.Samples.CursorControl.MainWindow();
+            //cursor.Show();
+            info.UseShellExecute = true;
+            info.FileName = "setup.exe";
+            info.WorkingDirectory = @"C:\Users\danis\Desktop\MouseKinect";
+
+            //Process.Start(info);
+            CapitalesMexico.MainWindow mex = new CapitalesMexico.MainWindow();
+            mex.ShowDialog();
+            //cursor.Close();
+            this.OnLoaded(this, new RoutedEventArgs());
+
+
+
+        }
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+
+            _sensorChooser.Stop();
+
+            //System.Diagnostics.Process.Start(@"C:\Users\danis\Desktop\MouseKinect\setup.exe");
+            //ProcessStartInfo info = new ProcessStartInfo();
+
+            info.UseShellExecute = true;
+            info.FileName = "setup.exe";
+            info.WorkingDirectory = @"C:\Users\danis\Desktop\MouseKinect";
+
+            //Microsoft.Kinect.Samples.CursorControl.MainWindow cursor = new Microsoft.Kinect.Samples.CursorControl.MainWindow();
+            //cursor.Show();
+
+            //Process.Start(info);
+            america.MainWindow america = new america.MainWindow();
+            america.ShowDialog();
+            //cursor.Close();
+            this.OnLoaded(this, new RoutedEventArgs());
+            
+        }
+    }
+}
